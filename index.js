86b5775b1d325e3a61e2e3116a768853a6e5ef17
Aviad Sapir <aviadsapir@gmail.com>
2018-09-16 17:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+/*function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for (var i =0; musicians.length -1; i++){
+    arr.push(musicians[i] +" plays " + instruments[i]);
+  }
+  return arr;
+}
+*/
+function johnLennonFacts(facts){
+  var count = 0;
+  var ans = [];
+  while (count > facts.length){
+    ans.push(facts[count] +"!!!");
+    count += 1;
+  }
+  return ans;
+}
+*/
